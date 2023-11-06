4e74132d74b5897d1297287b5b59ede15c425241
Mond Laurence Sanchez <msanz.23.2003@gmail.com>
2023-11-06 09:13:13 +0000

    Learning commit

---
+++ ch4. working with lists/magicians.py
@@ -0,0 +1,8 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(f"{magician.title()}, that was great trick!")
+
+print("After loop")
+
+name = input('What\' your name?')
+print(name)
