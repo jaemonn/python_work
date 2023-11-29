cc01795322ce83d573b0aee5c2d8a53b7d78bd77
Mond Laurence Sanchez <msanz.23.2003@gmail.com>
2023-11-29 10:11:26 +0000

    learn python lsits

---
+++ ch4._working_with_lists/ex_4.13.py
@@ -0,0 +1,7 @@
+buffetFoods = ("spaghetti", "rice", "soup", "steak", "fried chicken")
+for food in buffetFoods :
+    print(food)
+
+buffetFoods = ("curry", "rice", "spring rolls", "steak", "fried chicken")
+for food in buffetFoods :
+    print(food)
---
+++ ch5_if_Statements/ex_5.1_5.2.py
@@ -0,0 +1,12 @@
+# food_1 = "Pizza"
+# food_2 = "bread"
+# print(food_1 == food_2)
+# print(food_1 != food_2)
+
+# print(food_1 == food_1.lower())
+
+# print(1 < 2 and 3 < 4)
+# print(1 == 2 or 1 > 0)
+
+food = ["apple", "rice", "soup", "steak"]
+print("chicken" not in food)
